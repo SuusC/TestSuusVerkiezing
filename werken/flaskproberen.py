0421097d106b162a7e89120115e0f9dbdc42380a
SuusC <suzan.suus.c@gmail.com>
2021-11-22 13:21:38 +0100

    3e test suus

--- werken/flaskproberen.py
+++ werken/flaskproberen.py
@@ -11,14 +11,32 @@ def hello_world():
         b = f.readline().split(';')
         a = ('\n<br>'.join(b))
         return a
-    return "<p>Hello, World!</p>"
+
 
 @app.route("/test",methods=['GET'])
 def test():
     df = pd.read_csv(r'C:\Users\suzan\Documents\traineeship\data\Uitslag_alle_gemeenten_TK20210317.csv', sep=';')
-    #d = pd.DataFrame(df)
     c = df.to_html()
     return c
+
+@app.route("/start",methods=['GET'])
+def hoi():
+    return "hoi"
+
+@app.route("/vvd",methods=['GET'])
+def vvdamsterdam():
+    df = pd.read_csv(r'C:\Users\suzan\Documents\traineeship\data\Uitslag_alle_gemeenten_TK20210317.csv', sep=';')
+    d = (df['VVD'][15])
+    e = str(d)
+    return e
+
+@app.route("/rangschikking/vvd",methods=['GET'])
+def rankschikking():
+    df = pd.read_csv(r'C:\Users\suzan\Documents\traineeship\data\Uitslag_alle_gemeenten_TK20210317.csv', sep=';')
+    f = (df[['RegioNaam', 'VVD']])
+    g = f.sort_values(by=['VVD'])
+    return str(g)
+
 
 
 if __name__ == '__main__':
--- werken/test.py
+++ werken/test.py
@@ -1,5 +1,8 @@
 import pandas as pd
 
+
 df = pd.read_csv(r'C:\Users\suzan\Documents\traineeship\data\Uitslag_alle_gemeenten_TK20210317.csv', sep=';')
-#b = df.to_html()
-print(df.head(3))
+f = (df[['RegioNaam','VVD']])
+g = f.sort_values(by=['VVD'])
+print(g)
+
